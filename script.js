79b5bc202f13a53c0d11d17a0adcb8ac09208904
Alan Foote <92006161+alanfoote@users.noreply.github.com>
2021-11-19 11:45:29 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var map = L.map('map').fitWorld();
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 10,
+    id: 'mapbox/streets-v8',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiYWZvb3RlIiwiYSI6ImNrdzMweGtuZDVwZjkyb28wY2I4OTJiNmIifQ.ZbhPEs7coJbMMysypZc5CQ',
+}).addTo(map);
+function onLocationFound(e) {
+    var radius = e.accuracy; //this defines a variable radius as the accuracy value returned by the locate method. The unit is meters.
+
+    L.marker(e.latlng).addTo(map)  //this adds a marker at the lat and long returned by the locate function.
+        .bindPopup("You are within " + Math.round(radius * 3.28084) + " feet of this point").openPopup(); //this binds a popup to the marker. The text of the popup is defined here as well. Note that we multiply the radius by 3.28084 to convert the radius from meters to feet and that we use Math.round to round the conversion to the nearest whole number.
+
+    L.circle(e.latlng, radius).addTo(map); //this adds a circle to the map centered at the lat and long returned by the locate function. Its radius is set to the var radius defined above.
+          }
+    map.on('locationfound', onLocationFound); //this is the event listener
+
+    function onLocationError(e) {
+      alert(e.message);
+    }
+    map.on('locationfound', onLocationFound); //this is the event listener
+    map.on('locationerror', onLocationError);
+    map.locate({setView: true, maxZoom: 10});
