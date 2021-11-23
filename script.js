ac6f0927e1fed3c17c6c4a1ef0ace796551b81bc
Alan Foote <92006161+alanfoote@users.noreply.github.com>
2021-11-23 10:38:19 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
-var map = L.map('map').fitWorld();
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+var light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 10,
-    id: 'mapbox/streets-v8',
+    maxZoom: 18,
+    id:'mapbox/light-v10',
+    accessToken: 'pk.eyJ1IjoiYWZvb3RlIiwiYSI6ImNrdzMweGtuZDVwZjkyb28wY2I4OTJiNmIifQ.ZbhPEs7coJbMMysypZc5CQ',
     tileSize: 512,
     zoomOffset: -1,
+});
+
+var dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id:'mapbox/dark-v10',
     accessToken: 'pk.eyJ1IjoiYWZvb3RlIiwiYSI6ImNrdzMweGtuZDVwZjkyb28wY2I4OTJiNmIifQ.ZbhPEs7coJbMMysypZc5CQ',
-}).addTo(map);
+    tileSize: 512,
+    zoomOffset: -1,
+});
+var map = L.map('map', {layers:[light]}).fitWorld();
+
+
+// onclick //
 function onLocationFound(e) {
     var radius = e.accuracy; //this defines a variable radius as the accuracy value returned by the locate method. The unit is meters.
 
     L.marker(e.latlng).addTo(map)  //this adds a marker at the lat and long returned by the locate function.
         .bindPopup("You are within " + Math.round(radius * 3.28084) + " feet of this point").openPopup(); //this binds a popup to the marker. The text of the popup is defined here as well. Note that we multiply the radius by 3.28084 to convert the radius from meters to feet and that we use Math.round to round the conversion to the nearest whole number.
 
-    L.circle(e.latlng, radius).addTo(map); //this adds a circle to the map centered at the lat and long returned by the locate function. Its radius is set to the var radius defined above.
-          }
+        if (radius <= 100) {
+            L.circle(e.latlng, radius, {color: 'green'}).addTo(map);
+            }
+        else{
+            L.circle(e.latlng, radius, {color: 'red'}).addTo(map);
+            } //this adds a circle to the map centered at the lat and long returned by the locate function. Its radius is set to the var radius defined above.
+            var times = SunCalc.getTimes(new Date(), e.latitude, e.longitude);
+  var sunrise = times.sunrise.getHours();
+  var sunset = times.sunset.getHours();
+
+  var currentTime = new Date().getHours();
+      if (sunrise < currentTime && currentTime < sunset){
+        map.removeLayer(dark);
+        map.addLayer(light);
+      }
+      else {
+        map.removeLayer(light);
+        map.addLayer(dark);
+      }
+    }
     map.on('locationfound', onLocationFound); //this is the event listener
 
     function onLocationError(e) {
@@ -24,3 +54,7 @@ function onLocationFound(e) {
     map.on('locationfound', onLocationFound); //this is the event listener
     map.on('locationerror', onLocationError);
     map.locate({setView: true, maxZoom: 10});
+    var baseMaps = {
+         "dark": dark,
+         "light": light
+    }};
